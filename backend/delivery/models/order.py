d4f720fa1ed485df6db4eb4c6f9ca549cc6dae18
Miguel <miguel.m.frutuoso@hotmail.com>
2021-11-14 02:44:06 +0000

    contraint null

--- backend/delivery/models/order.py
+++ backend/delivery/models/order.py
@@ -20,7 +20,7 @@ class Order(models.Model):
     retailer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='retailer')
     date_publish = models.DateField(default=datetime.date.today)
     date_available = models.DateField()
-    date_delivery = models.DateField()
+    date_delivery = models.DateField(null=True)
     weight = models.DecimalField(decimal_places=2, max_digits=5)
     description = models.CharField(max_length=500, null=True)
     route = models.ForeignKey(Route, related_name='orders', related_query_name="orders", null=True, on_delete=models.CASCADE)
--- backend/delivery/models/order_timelocation.py
+++ backend/delivery/models/order_timelocation.py
@@ -1,10 +1,15 @@
 from django.db import models
 from .order import Order
+from django.db.models import Q
 
 class orderTimelocation(models.Model):
     longitude = models.DecimalField(decimal_places=16, max_digits=20)
     latitude = models.DecimalField(decimal_places=16, max_digits=20)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='ordertimelocation', related_query_name="ordertimelocation", null=True)
-    selected = models.BooleanField(default=False)
+    selected = models.BooleanField(null=True)
     nth_order = models.IntegerField(null=True)
     
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=(Q(selected__isnull = True) & Q(nth_order__isnull = True)) | (Q(selected__isnull = False) & Q(nth_order__isnull = False)), name='select_nth_order')
+        ]
