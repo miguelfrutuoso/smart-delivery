2fe9d1a54ebc7ca71d48711314fed25b105e98cb
Miguel <miguel.m.frutuoso@hotmail.com>
2021-11-16 19:02:30 +0000

    driver added to route model

--- backend/delivery/models/route.py
+++ backend/delivery/models/route.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.conf import settings
 import datetime
 from .warehouse import Warehouse
+from django.db.models import Q
 
 class Route(models.Model):
     day = models.DateField()
     warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE) 
     start_time = models.TimeField()
+    driver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, related_name='driver')
